6d464a4a8748ea3582c43a4be68a94d1069a3a80
MishaKravchenko <mishakravchenko.1998@gmail.com>
2023-01-17 12:54:04 +0200

    initial state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import './App.css';
-import {Simpsons} from "./components/Simpsons/Simpsons";
-import {RickMortyList} from "./components/RickMortyList/RickMortyList";
 
 function App() {
     return (
         <div className="App">
-            <div className="simpsons_wrapper">
-                <h1>Simpsons</h1>
-                <Simpsons/>
-            </div>
 
-            <div className="rickMorty_wrapper">
-                <h1>Rick & Morty</h1>
-                <RickMortyList/>
-            </div>
         </div>
     );
 }
