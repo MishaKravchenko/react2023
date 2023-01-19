648f786e9785d0b03975a48e8626a54396782306
MishaKravchenko <mishakravchenko.1998@gmail.com>
2023-01-19 11:20:47 +0200

    UserDetails + UserPosts

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -4,7 +4,7 @@ import './UserDetails.css'
 const UserDetails = ({user: {id, name, username, address: {street, city},phone, email},getUserId}) => {
     return (
         <div>
-            <h2>{id}. {name} {username}</h2>
+            <h2>↓ {id}. {name} {username} ↓</h2>
             <p><b>Street:</b> {street}</p>
             <p><b>City:</b> {city}</p>
             <p><b>Email:</b> {email}</p>
